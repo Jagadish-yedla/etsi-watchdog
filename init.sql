e9805a1a5da8f8a4254f1f788bc91ae5dae0e98c
Jagadish-yedla <akhilkumaryedla143@gmail.com>
2025-08-14 00:26:20 +0530

    saving work before rebase

--- init.sql
+++
@@ -1,4 +0,0 @@
--- init.sql (placed in project root or referenced by docker-compose)
-CREATE USER etsi_user WITH PASSWORD 'password';
-CREATE DATABASE drift_monitor;
-GRANT ALL PRIVILEGES ON DATABASE drift_monitor TO etsi_user;
